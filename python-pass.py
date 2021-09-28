1fca3923fbaf5ece90fd62f46320ed9ccb6ee480
osamah mohammedridha <osamaalrsam43@gmail.com>
2021-09-28 22:56:03 +0300

    first commit

--- python-pass.py
+++ python-pass.py
@@ -1,7 +1,28 @@
 
+def longestPalindrome(s:str)->str:
+        res = ""
+        resLen=0
+        
+        for i in range(len(s)):
+            #odd Length
+            l,r=i,i
+            while l >= 0 and r < len(s) and s[l] == s[r]:
+                if ( r - l + 1 ) > resLen:
+                    res = s[l:r+1]
+                    resLen = r-l+1
+                l -= 1
+                r += 1
+            
+            #even Length
+            l,r=i,i+1
+            while l >= 0 and r < len(s) and s[l] == s[r]:
+                if ( r - l + 1 ) > resLen:
+                    res = s[l: r + 1]
+                    resLen = r - l + 1
+                l -= 1
+                r += 1
+        
+        return res
 
-class Solution:
 
-    @staticmethod
-    def longest_palindromic(s: str) -> str:
-        pass
+print(longestPalindrome("babad"))
